981d77f107f7b0d0eb69ffd0a5271bcf0df679f6
joelantonio86 <joelantoniomg.86@gmail.com>
2024-12-12 02:08:19 -0300

    corrigindo itens minimizados

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-const musicListItems=document.querySelectorAll(".music-list li");musicListItems.forEach(a=>{a.addEventListener("mouseover",()=>{const b=a.querySelector(".instruments");b.style.display="block"}),a.addEventListener("mouseout",()=>{const b=a.querySelector(".instruments");b.style.display="none"})});
+const musicListItems = document.querySelectorAll(".music-list li");
+musicListItems.forEach((a) => {
+  a.addEventListener("mouseover", () => {
+    const b = a.querySelector(".instruments");
+    b.style.display = "block";
+  });
+  a.addEventListener("mouseout", () => {
+    const b = a.querySelector(".instruments");
+    b.style.display = "none";
+  });
+});
+
+const musicItems = document.querySelectorAll(".music-list li");
+
+musicItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const instruments = item.querySelector(".instruments");
+    instruments.style.display = "block";
+  });
+});
